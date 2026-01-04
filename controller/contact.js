75b6acf8b8e9c4a47a344394035fdcb46cc729ad
Pratik Rathore <jipratik49@gmail.com>
2026-01-04 09:17:37 +0530

    feat: replace nodemailer with resend for email handling and update CORS policy

--- controller/contact.js
+++ controller/contact.js
@@ -1,10 +1,9 @@
 import Contact from "../models/contact.js";
-import { transporter } from "../config/mail.js";
-import dotenv from 'dotenv'
-dotenv.config()
+import resend from "../config/resend.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const contactDetails = async (req, res) => {
-  console.log("running")
   try {
     const { name, email, message, subject } = req.body;
     if (!name || !email || !message || !subject) {
@@ -12,33 +11,38 @@ const contactDetails = async (req, res) => {
         .status(400)
         .json({ success: false, message: "All Fields are Required" });
     }
-  const savedContact =   await Contact.create({ name, email, message, subject });
-       console.log("mere process",process.env.EMAIL_USER,"dfdfd")
-       console.log("save",savedContact)
-    // await transporter.sendMail({
-    //   from: `"Portfolio Contact",<${process.env.EMAIL_USER}>`,
-    //   to: process.env.EMAIL_USER,
-    //   replyTo: email,
-    //   subject: `New Portfolio Message: ,${subject}`,
-    //   html: `
-    //     <h3>New Contact Message</h3>
-    //     <p><strong>Name:</strong> ${name}</p>
-    //     <p><strong>Email:</strong> ${email}</p>
-    //     <p><strong>Message:</strong> ${message}</p>
-    //   `,
-    // });
-//     await transporter.sendMail({
-//       from: `"Pratik Rathore" <${process.env.EMAIL_USER}>`,
-//       to: email,
-//       subject: "Thanks for connecting with me!",
-//       html: `
-//   <p>Hey, I'm <strong>Pratik</strong> 👋</p>
-//   <p>Thanks for connecting with me through my portfolio.</p>
-//   <p>I've received your message and will get back to you shortly.</p>
-//   <br/>
-//   <p>Best regards,<br/>Pratik Rathore</p>
-// `,
-//     });
+    const savedContact = await Contact.create({
+      name,
+      email,
+      message,
+      subject,
+    });
+
+    await resend.emails.send({
+      from: `Portfolio Contact <${process.env.EMAIL_FROM}>`,
+      to: process.env.EMAIL_FROM,
+      subject: `New Portfolio Message: ${subject}`,
+      html: `
+        <h3>New Contact Message</h3>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Message:</b> ${message}</p>
+      `,
+    });
+
+  
+    await resend.emails.send({
+      from: `Pratik Rathore <${process.env.EMAIL_FROM}>`,
+      to: email,
+      subject: "Thanks for connecting with me!",
+      html: `
+        <p>Hi ${name},</p>
+        <p>Thanks for contacting me through my portfolio.</p>
+        <p>I’ve received your message and will reply soon.</p>
+        <br/>
+        <p>Regards,<br/>Pratik Rathore</p>
+      `,
+    });
 
     return res.status(201).json({
       success: true,
@@ -48,7 +52,7 @@ const contactDetails = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: error.message || 'Error',
+      message: error.message || "Error",
     });
   }
 };
